2c6ad839bda19d8a9dcbbfd74975ee34b1fcad07
Bah <gadhatto1995@gmail.com>
2024-01-08 23:54:41 -0500

    webapi – commit initial

---
+++ src/Web2.API/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace Web2.API
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public   string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ src/Web2.API/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    static List<Product> products = new List<Product>()
+    {
+        new Product {Id = 1, Name = "Iphone 12"},
+        new Product {Id = 2, Name = "Iphone 15"},
+        new Product {Id = 3, Name = "Iphone 14"}
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> Get()
+    {
+        return Ok(products);
+    }
+
+}
+
